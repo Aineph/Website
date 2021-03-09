f72f964e53b1de3a5946507a81819a969a82b91e
Faintler <nicolas.fez@epitech.eu>
2021-03-09 22:11:27 +0100

    [UPDATE] Revert index.php.

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
-
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {
